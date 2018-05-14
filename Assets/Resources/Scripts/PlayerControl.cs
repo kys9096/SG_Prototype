0271ec4bbf08a6974a3677db42038d5fec760133
kys9096 <kys9096@naver.com>
2018-05-14 20:06:51 +0900

    스크립트 추가

---
+++ Assets/Resources/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public int abc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void temp()
+    {
+        //asdfasdfasdasfasd
+    }
+}
