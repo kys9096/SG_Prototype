11a81c814fc6769baae5d01af6aa01c7afba9947
lailael <sssd00123@naver.com>
2018-05-14 20:09:30 +0900

    SphereControl 바꿈 ㅋ

--- Assets/Resources/Scripts/SphereControl.cs
+++ Assets/Resources/Scripts/SphereControl.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SphereControl : MonoBehaviour {
 
+    private bool isColllided;
+
 	// Use this for initialization
 	void Start () {
-		
+        isColllided = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void ColWithAnithing()
+    {
+        isColllided = true;
+    }
 }
